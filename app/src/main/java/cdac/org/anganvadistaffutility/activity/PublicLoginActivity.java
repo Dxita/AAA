b7275b738c4b4ba3c072e7cf0dd39231f9c6bb35
Dxita <dixitakhangarot21@gmail.com>
2020-10-06 18:14:15 +0530

    Public Side- Login & Register screens designed

--- app/src/main/java/cdac/org/anganvadistaffutility/activity/PublicLoginActivity.java
+++ app/src/main/java/cdac/org/anganvadistaffutility/activity/PublicLoginActivity.java
@@ -1,30 +1,42 @@
 package cdac.org.anganvadistaffutility.activity;
 
+import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatTextView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 import cdac.org.anganvadistaffutility.R;
 
 public class PublicLoginActivity extends BaseActivity implements View.OnClickListener {
     AppCompatTextView register_txt;
+    AppCompatButton login_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_public_login);
+        login_btn=findViewById(R.id.login_btn);
         register_txt=findViewById(R.id.register_txt);
         register_txt.setOnClickListener(this);
+        login_btn.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId()){
-            case R.id.login_txt:
+            case R.id.register_txt:
                 startActivity(new Intent(context, PublicRegisterationActivity.class));
                 break;
+            case R.id.login_btn:
+                startActivity(new Intent(context, ViewInfraStructureActivity.class));
+                break;
+
         }
     }
 }
--- app/src/main/java/cdac/org/anganvadistaffutility/activity/PublicRegisterationActivity.java
+++ app/src/main/java/cdac/org/anganvadistaffutility/activity/PublicRegisterationActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.widget.AppCompatTextView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 
 import cdac.org.anganvadistaffutility.R;
 
@@ -13,6 +14,9 @@ AppCompatTextView login_txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_public_registeration);
         login_txt=findViewById(R.id.login_txt);
         login_txt.setOnClickListener(this);
