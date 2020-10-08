0436873d179f96f191039eb19be9c2f91d9c530c
Dxita <dixitakhangarot21@gmail.com>
2020-10-08 09:41:03 +0530

    PublicInfraActivity, InfrastructureActivity, BuildingDetailsActivity designed-D

--- app/src/main/java/cdac/org/anganvadistaffutility/activity/InfrastructureActivity.java
+++ app/src/main/java/cdac/org/anganvadistaffutility/activity/InfrastructureActivity.java
@@ -18,8 +18,7 @@ public class InfrastructureActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 
         setContentView(R.layout.activity_infrastructure);
 
