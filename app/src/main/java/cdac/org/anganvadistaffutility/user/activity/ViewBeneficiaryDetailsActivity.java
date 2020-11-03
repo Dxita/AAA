f5d55fc03ee474b61593f0110de5cc72f3318f18
hemant-android12 <hemantshpandey@gmail.com>
2020-11-03 18:06:44 +0530

    Spinner

--- app/src/main/java/cdac/org/anganvadistaffutility/user/activity/ViewBeneficiaryDetailsActivity.java
+++ app/src/main/java/cdac/org/anganvadistaffutility/user/activity/ViewBeneficiaryDetailsActivity.java
@@ -3,12 +3,18 @@ package cdac.org.anganvadistaffutility.user.activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatButton;
 import androidx.appcompat.widget.AppCompatEditText;
 
+import com.chivorn.smartmaterialspinner.SmartMaterialSpinner;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import cdac.org.anganvadistaffutility.R;
@@ -34,9 +40,37 @@ public class ViewBeneficiaryDetailsActivity extends BaseActivity implements View
         edt_janadhar_id = findViewById(R.id.edt_janadhar_id);
         edt_bhamashah_id = findViewById(R.id.edt_bhamashah_id);
 
+        initBeneficiaryCriteriaSpinner();
+
         AppCompatButton btn_search = findViewById(R.id.btn_search);
         btn_search.setOnClickListener(this);
     }
+
+    private void initBeneficiaryCriteriaSpinner() {
+        SmartMaterialSpinner<String> sp_beneficiary_criteria = findViewById(R.id.sp_provinces);
+        List<String> beneficiaryCriteriaList = new ArrayList<>();
+
+        beneficiaryCriteriaList.add("Pregnant");
+        beneficiaryCriteriaList.add("ABC");
+        beneficiaryCriteriaList.add("DEF");
+        beneficiaryCriteriaList.add("GHI");
+        beneficiaryCriteriaList.add("JKL");
+        beneficiaryCriteriaList.add("MNO");
+
+        sp_beneficiary_criteria.setItem(beneficiaryCriteriaList);
+
+        sp_beneficiary_criteria.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                Toast.makeText(context, beneficiaryCriteriaList.get(position), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+    }
+
 
     @Override
     public void onClick(View view) {
